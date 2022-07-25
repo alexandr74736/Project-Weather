1aee8f9f882cf45d37f54948384eef4313276a4e
Alexander <a17230@yandex.ru>
2022-07-25 18:28:50 +0400

    Basic PWA added

---
+++ src/app/pwa/pwa.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pwa',
+  templateUrl: './pwa.component.html',
+  styleUrls: ['./pwa.component.css']
+})
+export class PwaComponent {
+
+  constructor() { 
+    
+  }
+
+  
+
+}
