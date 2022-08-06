40d8af1d51e350915050f6e215c291df5dcee071
Alexander <a17230@yandex.ru>
2022-08-06 16:48:18 +0400

    the basic version of the MAIN has been completed

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,83 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { City } from '../shared/city';
+import { HttpService } from '../services/http.service';
+import { localService } from '../services/local.service';
+import { PostsService } from '../services/posts.service';
+import { autocompleeteService } from '../services/autocompleete.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
+  providers: [ autocompleeteService]
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  posts: City[] | any = [] 
+  post: any
 
+  pSub?: Subscription 
+  dSub?: Subscription 
+
+  keyword = 'name';
+  data = this.acService.data
+  arr: any
+  
+
+  constructor(
+                private localService: localService,
+                private httpService: HttpService,
+                private postsService: PostsService,
+                private acService: autocompleeteService,
+                private router: Router
+              ) { }
+
+   
   ngOnInit(): void {
+
+    this.dSub = this.postsService.bSbj$.subscribe(arr => {
+      this.arr = arr
+
+      if (this.arr.length == 0) {
+        this.posts = []
+        let locArr = this.localService.getAll();
+        locArr.forEach( (item) => {
+          this.posts.push(item)
+        })
+      } else {
+        arr.forEach( (item) => {
+          let name = item["name"]
+          this.posts = []
+          return this.httpService.getCityData(name).subscribe((data: City) => {
+            this.post=data
+            this.posts.push(this.post)
+          })
+        })
+      }
+    })
+    
+  }
+  
+  selectEvent(item:any) {
+    const acResponse: string = item.name
+    this.acService.newSub(acResponse)
+    this.router.navigate( ['/page', acResponse] )
+  }
+
+  goToCity($event:any) {
+    let e = $event.currentTarget.firstElementChild.firstElementChild.innerHTML
+    this.router.navigate( ['/page', e] )
+  }
+    
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
+    if (this.dSub) {
+      this.dSub.unsubscribe()
+    }
   }
 
 }
